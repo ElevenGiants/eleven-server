4b0ddf58f33909707e578402b464acac5cd45646
Markus Dolic <crowley@gmx.net>
2014-10-22 13:28:26 +0200

    fix infinite loop when loading a player
    When loading certain objects with recursive objrefs (e.g. a player with
    items in the inventory), the check whether a subordinate item is
    RPC-local runs into an infinite loop (because it tries loading the
    parent object again which is not actually loaded yet, etc.).
    
    This is fixed here by adding objects to the RC cache *while* they are
    being loaded, which is a bit hacky and would not be necessary if the
    RPC-local test did not require fully loading objects in certain cases
    (e.g.: item local? -> owner (player) local? -> player must be
    loaded to check if location is local).
    
    Added a test to verify the fix, and made persistence layer functional
    tests less dependent on mock objects in the process.

--- src/data/pers.js
+++ src/data/pers.js
@@ -95,14 +95,18 @@ function load(tsid) {
 	assert(typeof data === 'object', 'no or invalid data for ' + tsid);
 	orProxy.proxify(data);
 	var obj = gsjsBridge.create(data);
+	// store in request cache (necessary to prevent infinite loops when loading
+	// structures with circular objref dependencies, e.g. Players with Items)
+	RC.getContext().cache[tsid] = obj;
 	if (!rpc.isLocal(obj)) {
-		// wrap object in RPC proxy and add it to request cache
+		// wrap object in RPC proxy and update request cache reference
 		obj = rpc.makeProxy(obj);
 		RC.getContext().cache[tsid] = obj;
 	}
 	else {
 		// make sure any changes to the object are persisted
 		obj = persProxy.makeProxy(obj);
+		RC.getContext().cache[tsid] = obj;
 		// send onLoad event if there is a handler
 		if (obj.onLoad) {
 			obj.onLoad();
--- test/func/data/pers.js
+++ test/func/data/pers.js
@@ -1,14 +1,13 @@
 'use strict';
 
 var path = require('path');
-var rewire = require('rewire');
-var pers = rewire('data/pers');
+var pers = require('data/pers');
 var gsjsBridge = require('model/gsjsBridge');
 var GameObject = require('model/GameObject');
+var Player = require('model/Player');
 var Item = require('model/Item');
 var pbeMock = require('../../mock/pbe');
-var rpcMock = require('../../mock/rpc');
-var rcMock = require('../../mock/RequestContext');
+var RC = require('data/RequestContext');
 
 
 suite('pers', function () {
@@ -29,29 +28,34 @@ suite('pers', function () {
 	});
 
 	setup(function (done) {
-		pers.__set__('rpc', rpcMock);
-		pers.__set__('RC', rcMock);
-		rcMock.reset();
 		pers.init(pbeMock, FIXTURES_PATH, done);
 	});
 
 	teardown(function () {
-		pers.__set__('rpc', require('data/rpc'));
-		pers.__set__('RC', require('data/RequestContext'));
-		rcMock.reset();
 		pers.init();  // disable mock back-end
 	});
 
-
 	suite('game object loading', function () {
 
-		test('loaded game objects are initialized correctly', function () {
-			var o = pers.get('IHFK8C8NB6J2FJ5');
-			assert.instanceOf(o, Item);
-			assert.instanceOf(o, GameObject);
-			assert.strictEqual(o.constructor.name, o.class_tsid);
-			assert.property(o, 'distributeQuoinShards', 'quoin-specific property');
-			assert.property(o, 'distanceFromPlayer', 'property from item.js');
+		test('loaded game objects are initialized correctly', function (done) {
+			new RC().run(function () {
+				var o = pers.get('IHFK8C8NB6J2FJ5');
+				assert.instanceOf(o, Item);
+				assert.instanceOf(o, GameObject);
+				assert.strictEqual(o.constructor.name, o.class_tsid);
+				assert.property(o, 'distributeQuoinShards', 'quoin-specific property');
+				assert.property(o, 'distanceFromPlayer', 'property from item.js');
+			}, done);
+		});
+
+		test('recursive structures (Player with inventory) are loaded correctly',
+			function (done) {
+			new RC().run(function () {
+				var p = pers.get('P00000000000002');
+				assert.instanceOf(p, Player);
+				assert.deepEqual(Object.keys(p.items), ['I00000000000002']);
+				assert.isTrue(p.items.I00000000000002.__isORP);
+			}, done);
 		});
 	});
 });
