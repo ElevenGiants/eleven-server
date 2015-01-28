360618b99a368b62c53c0b6a4b05c3a19103b842
Markus Dolic <crowley@gmx.net>
2015-01-28 17:37:38 +0100

    prevent timers on unloaded objects from firing
    Mark objects with a 'stale' flag when unloading them, so
    timer/interval calls can detect when they are firing on an already
    unloaded objects and abort the call. This is a safeguard for
    situations that really should not be occurring in the first place.

--- src/data/RequestContext.js
+++ src/data/RequestContext.js
@@ -8,6 +8,7 @@ var util = require('util');
 var wait = require('wait.for');
 var Fiber = require('wait.for/node_modules/fibers');
 var pers = require('data/pers');
+var utils = require('utils');
 
 
 /**
@@ -165,6 +166,7 @@ RequestContext.prototype.setDirty = function setDirty(obj) {
  * @param {GameObject} obj
  */
 RequestContext.prototype.setUnload = function setUnload(obj) {
+	utils.addNonEnumerable(obj, 'stale', true);
 	this.unload[obj.tsid] = obj;
 };
 
