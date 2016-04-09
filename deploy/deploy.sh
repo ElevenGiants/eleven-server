ab7edc97355a5da1ce8a7019d805b295e673aaec
Markus Dolic <crowley@gmx.net>
2016-04-09 14:34:00 +0200

    Make the SSH user for remote deployment configurable
    * allow specifying the SSH user to log in on the target system during
      deployment through the environment variable `SSH_USER`
    * adjust the RethinkDB systemd unit name to be instance specific (as
      suggested in docs, see https://rethinkdb.com/docs/start-on-startup/)

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,8 +13,8 @@ if [[ ! -d "$src_dir" ]]; then
 fi
 
 # copy everything over to target host
-rsync ${DRY_RUN:+--dry-run} --compress --recursive --exclude=".git*" -e "ssh -p ${SSH_PORT}" "${src_dir}/" jenkins@${SSH_HOST}:/eleven/eleven-server.new
+rsync ${DRY_RUN:+--dry-run} --compress --recursive --exclude=".git*" -e "ssh -p ${SSH_PORT}" "${src_dir}/" ${SSH_USER}@${SSH_HOST}:/eleven/eleven-server.new
 
 # run remote deployment script (moves old version out of the way, replaces it
 # with new one and restarts GS service)
-ssh -T -p ${SSH_PORT} jenkins@${SSH_HOST} /eleven/eleven-server.new/deploy/deploy-remote.sh
+ssh -T -p ${SSH_PORT} ${SSH_USER}@${SSH_HOST} /eleven/eleven-server.new/deploy/deploy-remote.sh
