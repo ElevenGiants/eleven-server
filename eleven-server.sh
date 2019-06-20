53de3a38c34de5feeb4b4ae6d051bb7358196781
UmbraChimera <gitlab@umbrachimera.me>
2019-06-20 14:40:04 +0000

    Delete eleven-server.sh
    Not needed as Vagrant build adds this

--- eleven-server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-PATH="/home/vagrant/.nvm/versions/node/v6.17.1/bin:$PATH" # manually set PATH for npm since sudo can't use npm
-cd /vagrant/eleven-server
-npm start || echo 'if node version is no longer v6.17.1 this script will fail and need to be updated' # error message to show in log on fail.
